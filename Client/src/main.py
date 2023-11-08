3c62779048867895c76df61df8394b16aa2f8ae3
NGODAT0103 <21521935@gm.uit.edu.vn>
2023-11-08 15:09:20 +0700

    test deploy

--- Client/src/main.py
+++ Client/src/main.py
@@ -2,10 +2,14 @@ import json
 import time
 import requests
 
-url_encoded_data = {
-    "type": "update_temp",
-    "temp": "100",
-    "time_primary": int(time.time())
-}
-request = requests.post("http://server.uitprojects.com/update_temp", data=url_encoded_data)
-print(request.json())
+
+while True:
+    url_encoded_data = {
+        "type": "update_temp",
+        "temp": input("Nhap nhiet do: "),
+        "time_primary": int(time.time())
+    }
+    if url_encoded_data["temp"]=="0":
+        break
+    request = requests.post("http://server.uitprojects.com/update_temp", data=url_encoded_data)
+    print(request.json())
