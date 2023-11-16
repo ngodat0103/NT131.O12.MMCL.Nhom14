8a3af62427fbdde104d268b0dfd1c4ee6db18f8a
NGODAT0103 <21521935@gm.uit.edu.vn>
2023-11-16 20:39:01 +0700

    fix thingsboard

--- Script/start_all.sh
+++ Script/start_all.sh
@@ -1,5 +1,7 @@
 docker network create -d bridge mynet2
 source mariadb/start.sh
-source thingsboard/start.sh
+tar -xzvf thingsboard/data.tar -C $(realpath thingsboard)
 source ../Server/start.sh
 source nginx/start.sh
+cd thingsboard/
+./start.sh
