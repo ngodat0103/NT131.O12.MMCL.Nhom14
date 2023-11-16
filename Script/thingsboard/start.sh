38bd63ce8b1fa0f922147b49e0cff5e8daf17294
NGODAT0103 <21521935@gm.uit.edu.vn>
2023-11-16 19:57:51 +0700

    fix thingsboard

--- Script/thingsboard/start.sh
+++
@@ -1,4 +0,0 @@
-sudo chown 799:799 -R .mytb-logs/
-sudo chown 799:799 -R .mytb-data/
-docker run -d --network=mynet2 -v $(realpath .mytb-data):/data \
--v $(realpath .mytb-logs):/var/log/thingsboard --name thingsboard2 --restart always thingsboard/tb-postgres
