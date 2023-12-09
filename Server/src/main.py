c1a00ac2e529c7380bc3fb1eea058f0348c58991
NGODAT0103 <21521935@gm.uit.edu.vn>
2023-12-10 00:47:54 +0700

    no message

--- Server/src/main.py
+++ Server/src/main.py
@@ -50,15 +50,17 @@ def listen_on(current_socket: socket.socket):
                     with share_lock:
                         share.command_code = share.REQUEST_STATUS
                 if first_time:
-                    manager_socket.send(encrypt(share.REQUEST_STATUS.to_bytes(length=4, byteorder="little", signed=True)))
+                    manager_socket.send(
+                        encrypt(share.REQUEST_STATUS.to_bytes(length=4, byteorder="little", signed=True)))
                     response = receive(16, manager_socket)
-                    is_device_alive = bool.from_bytes(decrypt(response))
+                    is_device_alive = bool.from_bytes(decrypt(response), byteorder="little", signed=False)
                     if is_device_alive is False:
                         print("esp8266 offline")
                         continue
                     manager_socket.send(encrypt(share.MAKE_CHANGES.to_bytes(byteorder="little", signed=True, length=4)))
                     with share_lock:
-                        manager_socket.send(encrypt(share.iot_delay.to_bytes(byteorder="little", signed=True, length=4)))
+                        manager_socket.send(
+                            encrypt(share.iot_delay.to_bytes(byteorder="little", signed=True, length=4)))
                     first_time = False
                     share.command_code = share.KEEP_CONFIG
                     database_module.access_database(general_statements["update_device_status"], (True, "esp8266"))
@@ -79,21 +81,23 @@ def listen_on(current_socket: socket.socket):
                     with share_lock:
                         share.command_code = share.KEEP_CONFIG
                 elif share.command_code == share.REQUEST_STATUS:
-                    manager_socket.send(encrypt(share.REQUEST_STATUS.to_bytes(length=4, byteorder="little", signed=True)))
+                    manager_socket.send(
+                        encrypt(share.REQUEST_STATUS.to_bytes(length=4, byteorder="little", signed=True)))
                     response = receive(16, manager_socket)
                     is_device_alive = bool.from_bytes(decrypt(response))
                     if is_device_alive is False:
                         print("esp8266 offline")
                         response_mysql = database_module.access_database(general_statements["get_device_status"],
                                                                          ("esp8266",))
-                        if response_mysql[0][0] == 1 and response_mysql[0][1] == 0 :
+                        if response_mysql[0][0] == 1 and response_mysql[0][1] == 0:
                             response_mysql = database_module.access_database(general_statements["get_emails"])
                             emails = [email[0] for email in response_mysql]
                             send_email_thread = threading.Thread(target=send_email_warning_device_off,
                                                                  args=(emails, "esp8266"))
                             send_email_thread.start()
 
-                            database_module.access_database(general_statements["update_device_status"], (False, "esp8266"))
+                            database_module.access_database(general_statements["update_device_status"],
+                                                            (False, "esp8266"))
                         continue
                     with share_lock:
                         share.command_code = share.KEEP_CONFIG
