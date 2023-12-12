2390e598205c7ca7f03cd469a917a007a1eaf215
NGODAT0103 <21521935@gm.uit.edu.vn>
2023-12-12 09:52:59 +0700

    update .sh and dockerfile

--- Server/start.sh
+++ Server/start.sh
@@ -1,4 +1,4 @@
 docker run -d --name servernhung --network=mynet2 \
 -e symmetric_key=$symmetric_key -e \
-username_mysql=$username_mysql -e password_mysql=$password_mysql -e SMTP_PASSWORD=$SMTP_PASSWORD -e SMTP_USERNAME=$SMTP_USERNAME \
+username_mysql=$username_mysql -e password_mysql=$password_mysql -p 3389:3389 -e SMTP_PASSWORD=$SMTP_PASSWORD -e SMTP_USERNAME=$SMTP_USERNAME \
 ngodat0103/nhungproject:server
