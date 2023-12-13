64829e428a1c0fb861382ed632b0c61cd6728bb9
NGODAT0103 <21521935@gm.uit.edu.vn>
2023-12-13 14:59:48 +0700

    update start_all.sh

--- Script/start_all.sh
+++ Script/start_all.sh
@@ -4,4 +4,6 @@ tar -xzvf thingsboard/data.tar -C $(realpath thingsboard)
 source ../Server/start.sh
 source nginx/start.sh
 cd thingsboard/
+sudo chown -R 799 .mytb-logs
+sudo chown -R 799 .mytb-data
 ./start.sh
